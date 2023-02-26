e24c211c4a45a0d8032d31257040e3b946b691b9
Siddhesh Morye <siddheshmorye309+sourcetree@gmail.com>
2023-02-26 13:53:36 +0530

    dynamic input features for classification layer

--- models/CNN.py
+++ models/CNN.py
@@ -1,4 +1,5 @@
 # Imports
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -8,19 +9,30 @@ class CNN(nn.Module):
 
         super(CNN, self).__init__()
 
+        # Input data 
         in_channels = input_shape[-1]
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=(3,3), stride=(1,1), padding=(1,1))
-        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
-        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), stride=(1,1), padding=(1,1))
-        self.fc1 = nn.Linear(16*7*7, num_classes)
+        self.num_classes = num_classes
+
+        # layers
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=(2,2), stride=(2,2)),
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        )
+
+        in_features = self.feature_extractor(torch.rand(1, *input_shape[::-1])).view(1, -1).shape[-1]
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features, num_classes)
+        )
 
     def forward(self, x):
 
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
-        x = x.reshape(x.shape[0], -1)
-        x = self.fc1(x)
+        x = self.feature_extractor(x)
+        x = self.classifier(x)
+
         return x
 
